ebbe9d8330eb6348bb656717398e8d80cebdbe16
Lingabothu Teja <101658970+lintez2002@users.noreply.github.com>
2026-06-28 12:24:57 +0530

    Add files via upload

---
+++ Hotel Application/src/main/java/com/cn/hotel/config/HotelSecurityConfig.java
@@ -0,0 +1,75 @@
+package com.cn.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class HotelSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception 
+	{
+		http
+			.authorizeHttpRequests(authorize -> authorize
+					.requestMatchers("/login").permitAll()
+					.anyRequest().authenticated()
+			)
+			
+			.oauth2Login(oauth2 -> oauth2
+					.loginPage("/login")
+					.defaultSuccessUrl("/hotel/getAll")
+			);
+		return http.build();
+	}
+
+	@Bean
+	public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+		return (authorities) -> {
+			Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+			authorities.forEach(authority -> {
+				if(OAuth2UserAuthority.class.isInstance(authority)) {
+
+					OAuth2UserAuthority oAuth2UserAuthority = (OAuth2UserAuthority) authority;
+					Map<String, Object> userAttributesMap = oAuth2UserAuthority.getAttributes();
+
+					if(userAttributesMap.containsKey("realm_access")) {
+						Map<String, Object> realmAccessMap = (Map<String, Object>) userAttributesMap.get("realm_access");
+						if(realmAccessMap.containsKey("roles")) {
+							List<String> roles = (List<String>)realmAccessMap.get("roles");
+							mappedAuthorities.addAll(roles.stream()
+									.map(role -> new SimpleGrantedAuthority("ROLE_"+role.toUpperCase()))
+									.collect(Collectors.toSet()));
+						}
+					}
+				}
+			});
+			return mappedAuthorities;
+		};
+	}
+
+
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return JwtDecoders.fromIssuerLocation("https://lemur-7.cloud-iam.com/auth/realms/key-cloak-hotel-demo");
+	}
+}
